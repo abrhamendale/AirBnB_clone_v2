09abfa5ec04e038340a8fb00a19e2555aaae5900
Abrham Endale <tmailae2020@gmail.com>
2023-01-23 15:45:11 -0800

    Added third task.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,15 +2,27 @@
 """
 Web_static
 """
+import os
 from fabric.api import *
+from fabric.operations import run, put, sudo
+env.hosts = ['54.242.195.19', '54.236.50.27']
+
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers
-    """
-    if archive_path is None:
+    """Deploys web_static to the web servers"""
+    if (os.path.isfile(archive_path) is False):
         return False
-    with cd ("/tmp"):
-        put("/versions/*" "./")
-    run ("tar -xf $(archive_path).tar -C /opt/files
-
+    try:
+        arcname = archive_path.split("/")[-1]
+        arcpath = ("/data/web_static/releases/" + arcname.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(arcpath))
+        run("sudo tar -xzf /tmp/{} -C {}".format(arcname, arcpath))
+        run("sudo rm /tmp/{}".format(arcname))
+        run("sudo mv {}/web_static/* {}/".format(arcpath, arcpath))
+        run("sudo rm -rf {}/web_static".format(arcpath))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(arcpath))
+        return True
+    except:
+        return False
