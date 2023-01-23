07530d5210eee1ee11ebedbd93640ca090dfdfe1
Abrham Endale <tmailae2020@gmail.com>
2023-01-23 15:55:40 -0800

    Added third task.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""
-Web_static
-"""
+"""Web_static"""
+from fabric.contrib import files
+from fabric.api import env, put, run
 import os
-from fabric.api import *
-from fabric.operations import run, put, sudo
+
 env.hosts = ['54.242.195.19', '54.236.50.27']
 
 
 def do_deploy(archive_path):
-    """Deploys web_static to the web servers"""
-    if (os.path.isfile(archive_path) is False):
+    """Deplos web_static on the web servers."""
+    if not os.path.exists(archive_path):
         return False
+    path = '/data/web_static/releases/'
+    dot = archive_path.split('.')[0]
+    slash = dot.split('/')[1]
+    arcname = path + slash
     try:
-        arcname = archive_path.split("/")[-1]
-        arcpath = ("/data/web_static/releases/" + arcname.split(".")[0])
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(arcpath))
-        run("sudo tar -xzf /tmp/{} -C {}".format(arcname, arcpath))
-        run("sudo rm /tmp/{}".format(arcname))
-        run("sudo mv {}/web_static/* {}/".format(arcpath, arcpath))
-        run("sudo rm -rf {}/web_static".format(arcpath))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(arcpath))
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(arcname))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(slash, arcname))
+        run('rm -f /tmp/{}.tgz'.format(slash))
+        run('mv {}/web_static/* {}/'.format(arcname, arcname))
+        run('rm -rf {}/web_static'.format(arcname))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(arcname))
         return True
     except:
         return False
