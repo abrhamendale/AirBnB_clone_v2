155ff654f8476d041925b577ec603e154c4ba3fd
Abrham Endale <tmailae2020@gmail.com>
2023-01-23 16:09:35 -0800

    Fixed more errors.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo mkdir -p ./data/web_static
 sudo mkdir -p ./data/web_static/releases
 sudo mkdir -p ./data/web_static/shared
 sudo mkdir -p ./data/web_static/releases/test
-echo "<h1>AirBnB</h1>" >> /data/web_static/releases/test/index.html
+echo "<h1>AirBnB</h1>" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
