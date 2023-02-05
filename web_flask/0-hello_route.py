23a8ac70442f4a9e9281da6fc31c65b7dec3b6b2
Abrham Endale <tmailae2020@gmail.com>
2023-02-05 12:35:56 -0800

    Ficed some errors.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""
-Hello route module.
-"""
-from flask import Flask
-hello = Flask(__name__)
+"""Hello route module."""
 
-@hello.route('/', strict_slashes=False)
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
 def hello_route():
+    """Hello route function"""
     return 'Hello, HBNB!'
+
+
+if __name__ == "__main__":
+    """Main function"""
+    app.run(host="0.0.0.0", port=5000)
