64f090f5d2bd5395866d4ec5228c97997a283a23
Abrham Endale <tmailae2020@gmail.com>
2023-02-05 14:15:13 -0800

    Fixed more errors.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Hello route module."""
+
+
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Hello route function"""
+    return 'Hello, HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Hello hbnb function"""
+    return 'HBNB'
+
+
+@app.route('/c/<user>', strict_slashes=False)
+def hello_c(user):
+    """Hello c function"""
+    return 'C %s' % user.replace("_", " ")
+
+
+@app.route('/python/(<user>)', strict_slashes=False)
+def hello_py(user):
+    """Python function"""
+    return 'Python %s' % user.replace("_", " ")
+
+
+if __name__ == "__main__":
+    """Main function"""
+    app.run(host="0.0.0.0", port=5000)
