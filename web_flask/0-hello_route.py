e8de21903cb59095938adbb8fb52330a067301ec
Abrham <abrhamendale9@gmail.com>
2023-04-02 13:44:31 +0000

    Modified hello_route file.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+sudo !/usr/bin/python3
 """Hello route module."""
 
 
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_route():
     """Hello route function"""
     return 'Hello, HBNB!'
